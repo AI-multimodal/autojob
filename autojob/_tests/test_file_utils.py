b18bc0de8d88ecc7b3aee7cf5bf6aad0b4b99ae4
Matthew Carbone <x94carbone@gmail.com>
2022-04-01 18:27:18 -0400

    Add test read and load json

--- autojob/_tests/test_file_utils.py
+++ autojob/_tests/test_file_utils.py
@@ -1,7 +1,13 @@
 from pathlib import Path
 from tempfile import TemporaryDirectory
+from uuid import uuid4
 
-from ..file_utils import exhaustive_directory_search, run_command
+from ..file_utils import (
+    exhaustive_directory_search,
+    run_command,
+    read_json,
+    save_json,
+)
 
 
 def test_exhaustive_directory_search(dummydirectories):
@@ -17,3 +23,16 @@ def test_run_command():
         ex = f"touch {str(name)}"
         run_command(ex)
         assert name.exists()
+
+
+def test_save_read_json():
+    dummy_dictionary = {str(uuid4()): str(uuid4()) for _ in range(10)}
+    with TemporaryDirectory() as tempdir:
+        name1 = Path(tempdir) / Path("tmp1.json")
+        save_json(dummy_dictionary, name1)
+        name2 = Path(tempdir) / Path("tmp2.json")
+        save_json(dummy_dictionary, name2, indent=2, sort_keys=True)
+        dummy_dictionary1 = read_json(name1)
+        dummy_dictionary2 = read_json(name2)
+    assert dummy_dictionary == dummy_dictionary1
+    assert dummy_dictionary == dummy_dictionary2
