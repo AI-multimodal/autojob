9b949035864cbd57b1ba8f7657efd94d0770a0bb
Matthew Carbone <x94carbone@gmail.com>
2022-04-01 18:23:03 -0400

    Add test run_command

--- autojob/_tests/test_file_utils.py
+++ autojob/_tests/test_file_utils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
-from ..file_utils import exhaustive_directory_search
+from ..file_utils import exhaustive_directory_search, run_command
 
 
 def test_exhaustive_directory_search(dummydirectories):
@@ -8,3 +9,11 @@ def test_exhaustive_directory_search(dummydirectories):
         d = exhaustive_directory_search(tmpdir, "submit.sbatch")
         for dd in d:
             assert "mp-390" not in Path(dd).parts[-2:]
+
+
+def test_run_command():
+    with TemporaryDirectory() as tempdir:
+        name = Path(tempdir) / Path("aintnothingbutaheartache.txt")
+        ex = f"touch {str(name)}"
+        run_command(ex)
+        assert name.exists()
