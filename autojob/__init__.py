a5acdc2d21be4fdc0361e3a318b6ea71a9a33da9
Matthew Carbone <x94carbone@gmail.com>
2022-04-07 20:09:32 -0400

    Add loguru logger, change config locations

--- autojob/__init__.py
+++ autojob/__init__.py
@@ -1,21 +1,67 @@
+"""The autojob code contains a complete set of default files based on the
+developers' primary use cases. These defaults are enumerated in this core
+module. The user can override any and all defaults by providing their own
+config files in `~/.autojob`. This directory will be created if it does not
+exist, and any config files in the defaults that do not exist in the directory
+will be created if they do not exist (TODO)."""
+
+from pathlib import Path
+import sys
+
+from loguru import logger
+
 from ._version import get_versions
 
 __version__ = get_versions()["version"]
 del get_versions
 
 
-CONFIG = {
-    "report.in": {
-        "FEFF": ["feff.inp"],
-        "VASP": ["INCAR", "POSCAR", "KPOINTS", "POTCAR"],
-    },
-    "report.out": {
-        "FEFF": [["xmu.dat", None], ["feff.out", "feff ends at"]],
-        "VASP": [
-            [
-                "OUTCAR",
-                " General timing and accounting informations for this job:",
-            ]
-        ],
-    },
-}
+# Set the logging style
+
+
+def generic_filter(names):
+    if names == "all":
+        return None
+
+    def f(record):
+        return record["level"].name in names
+
+    return f
+
+
+logger.remove(0)  # Remove the default logger
+
+FMT = "<w>{name}</>:<w>{function}</> [<lvl>{level}</>] <lvl>{message}</>"
+
+
+STDOUT_LOGGER_ID = logger.add(
+    sys.stdout,
+    colorize=True,
+    filter=generic_filter(["DEBUG", "INFO", "SUCCESS"]),
+    format=FMT,
+)
+
+STDERR_LOGGER_ID = logger.add(
+    sys.stderr,
+    colorize=True,
+    filter=generic_filter(["WARNING", "ERROR", "CRITICAL"]),
+    format=FMT,
+)
+
+ROOT = Path.home() / Path(".autojob")
+if not ROOT.exists():
+    ROOT.mkdir(exist_ok=False, parents=True)
+
+
+FMT2 = (
+    "<k>{time:YYYY-MM-DD HH:mm:ss.SSS}</> "
+    "<k>{name}</>:<k>{function}</>:<k>{line}</> "
+    "[<lvl>{level}</>] <lvl>{message}</>"
+)
+
+LOGFILE_LOGGER_ID = logger.add(
+    ROOT / Path("LOGS"),
+    filter=generic_filter("all"),
+    format=FMT2,
+    rotation="2.0 GB",
+)
